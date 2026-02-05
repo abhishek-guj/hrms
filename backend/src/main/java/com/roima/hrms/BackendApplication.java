b8f5ac941a1a602735502fa7e3daa5c87d8b1b9a
Abhishek Gujarathi <abhishek.gujarathi@roimaint.com>
2026-02-05 19:36:33 +0530

    setup: init

---
+++ backend/src/main/java/com/roima/hrms/BackendApplication.java
@@ -0,0 +1,13 @@
+package com.roima.hrms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
